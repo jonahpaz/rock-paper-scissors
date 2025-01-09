d8c337cab98b061cfafa105b22b79d9bffc5565b
Jonah Paz <jonicuspaz@outlook.com>
2025-01-09 01:05:03 -0500

    Add a UI to my rock paper scissors game

--- altScript.js
+++ altScript.js
@@ -1,18 +1,4 @@
-//Write the logic to get the computer choice:
-
-const choices = [ 'rock', 'paper', 'scissors' ];
-
-function getComputerChoice() {
-    let computerChoice = choices[Math.floor(Math.random()*3)];
-    return computerChoice
-}
-
-//Write the logic to get the human choice:
-
-function getHumanChoice() {
-    let humanChoice = prompt( 'Rock, paper or scissors?', 'rock' ).toLowerCase();
-    return humanChoice
-}
+const options = [ 'rock', 'paper', 'scissors' ];
 
 //Initialize the scores:
 
@@ -21,13 +7,56 @@ let computerScore = 0;
 
 //Write the logic to play a game round:
 
-function playRound(humanChoice,computerChoice) { 
+const buttons = document.querySelector(".buttons");
+const myImage = document.querySelector("#myChoice");
+const theirImage = document.querySelector("#theirChoice");
+
+buttons.addEventListener("click", (event) => {
+    let humanChoice = 
+    event.target.textContent.toLowerCase();
+    switch (humanChoice) {
+        case "rock":
+            myImage.setAttribute("src", "https://img.freepik.com/fotos-premium/piedra-natural-aislado-sobre-fondo-blanco_153912-10386.jpg");
+            break;
+        case "paper":
+            myImage.setAttribute("src", "https://st4.depositphotos.com/11634452/21820/i/450/depositphotos_218200610-stock-photo-white-paper-sheet-grid-line.jpg");
+            break;
+        case "scissors":
+            myImage.setAttribute("src", "https://www.parnassa.com.ar/product_images/b/197/tijera_7.5_.___86713_std.jpg");
+            break;
+    }
+});
+
+buttons.addEventListener("click", playRound);
+
+function playRound(event) {
 
     let roundResult;
+    let humanChoice = 
+        event.target.textContent.toLowerCase();
+    let computerChoice = 
+        options[Math.floor(Math.random()*3)];
 
-    if (humanChoice != 'rock' && humanChoice != 'paper' && humanChoice != 'scissors') {
-        roundResult = 'Please, enter a valid option.';
-    } else if (humanChoice === computerChoice) {
+        switch (computerChoice) {
+            case "rock":
+                theirImage.setAttribute("src", "https://img.freepik.com/fotos-premium/piedra-natural-aislado-sobre-fondo-blanco_153912-10386.jpg");
+                break;
+            case "paper":
+                theirImage.setAttribute("src", "https://st4.depositphotos.com/11634452/21820/i/450/depositphotos_218200610-stock-photo-white-paper-sheet-grid-line.jpg");
+                break;
+            case "scissors":
+                theirImage.setAttribute("src", "https://www.parnassa.com.ar/product_images/b/197/tijera_7.5_.___86713_std.jpg");
+                break;
+        }
+
+
+    let choices = 
+        [`Your choice: ${humanChoice}`, `Your opponent's choice: ${computerChoice}`]; 
+    console.log(choices);
+    const spanChoices = document.querySelector(".choices");
+    spanChoices.textContent = choices.join(" & ");
+
+    if (humanChoice === computerChoice) {
         roundResult = 'It is a tie :0';
     } else if (
         humanChoice === 'rock' && computerChoice === 'scissors' || 
@@ -40,33 +69,31 @@ function playRound(humanChoice,computerChoice) {
         roundResult = 'You lost :(';
     }
 
-    let selectedChoices = [humanChoice, computerChoice]; 
-    console.log(selectedChoices);
-
     if (roundResult === 'You won! :D') {
         humanScore++;
+
     } else if (roundResult === 'You lost :(') {
         computerScore++;
     }
 
-    return roundResult
-}
+    const score = [humanScore, computerScore];
+    const myScore = document.querySelector(".myScore");
+    const theirScore = document.querySelector(".theirScore");
+    myScore.textContent = score[0];
+    theirScore.textContent = score[1];
 
-//Play the game five times, keep track of the scores and declare a winner at the end.
+    let finalGameResult;
 
-for (let i = 0; i <= 4; i++) {
-    console.log( playRound( getHumanChoice(), getComputerChoice() ) );
-    console.log(`Human Score: ${humanScore}; Computer Score: ${computerScore}`);
-}
-
-let finalGameResult;
-
-if (humanScore === computerScore) {
-    finalGameResult = 'Nobody won';
-} else if (humanScore > computerScore) {
-    finalGameResult = 'You beat you opponent!';
-} else {
+if (humanScore === 5 && humanScore > computerScore) {
+    finalGameResult = 'You beat your opponent!';
+    humanScore = 0;
+    computerScore = 0;
+} else if (computerScore === 5 && computerScore > humanScore) {
     finalGameResult = 'You were defeated...';
+    humanScore = 0;
+    computerScore = 0;
 }
-
 console.log(finalGameResult);
+const spanFinal = document.querySelector(".final");
+spanFinal.textContent = finalGameResult;
+}
